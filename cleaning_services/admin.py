9efea3085d013b87df3f55f4534eadf769d024c6
Choyon <choyon.ugv@gmail.com>
2025-06-21 11:29:20 +0600

    start now

--- cleaning_services/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import BlogPost, Tag, Service, Comment
-
-# Register your models here.
-admin.site.register(BlogPost)
-admin.site.register(Tag)
-admin.site.register(Service)
-admin.site.register(Comment)
